d3289c1f9bb2f82bae8c4276383117831d2e7837
TheFireMonster <113089882+TheFireMonster@users.noreply.github.com>
2024-04-05 17:04:08 -0300

    ts try

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+console.log("Hello World!")
+import express, { Request, Response, NextFunction } from 'express';
+const app = express()
+import routes from './routes/routes'
+import path from 'path'
+import createError, { HttpError } from 'http-errors';
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.set('views', path.join(__dirname,'/views'))
+app.set('view engine', 'ejs')
+
+app.use('/public', express.static('public'))
+
+app.use(routes)
+
+app.get('/', function(req: Request, res: Response) {
+    res.redirect('/home')
+})
+
+app.use((req: Request, res: Response, next) => {
+    next(createError(404))
+})
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  if (!createError.isHttpError(err)) {
+      err = createError(500) as HttpError;
+  }
+
+  const statusCode = err.status || 500;
+  res.status(statusCode);
+  res.render(`${statusCode.toString()}`);
+});
+  
+app.listen(3000)
