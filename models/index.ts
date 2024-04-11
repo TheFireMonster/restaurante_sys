682744abbbab67ec0d8bbdd5d4440b331387f210
Gilson Cesar Carlos Filho <113089882+TheFireMonster@users.noreply.github.com>
2024-04-10 22:25:28 -0300

    dfddf

--- models/index.ts
+++ models/index.ts
@@ -1,13 +1,14 @@
 import fs from 'fs';
 import path from 'path';
-import Sequelize from 'sequelize';
+import {Sequelize} from 'sequelize';
 import process from 'process';
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
+let sequelize: String;
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
