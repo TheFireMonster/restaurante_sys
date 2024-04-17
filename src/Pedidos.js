9287cadea8bba27e9861707c2a22a909901fbe59
TheFireMonster <113089882+TheFireMonster@users.noreply.github.com>
2024-04-17 14:02:02 -0300

    upd and clean directories

--- src/Pedidos.js
+++
@@ -1,7 +0,0 @@
-function Calc(){
-    var Quant = document.getElementById('quantidade_produto').value;
-    var Valor = document.getElementById('valor_produto').value;
-    var Total = parseInt(Quant) * parseFloat(Valor);
-    document.getElementById('total').innerHTML=Total+" R$"
-}
-
