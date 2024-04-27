f4fe438da9b39341286c2cf8dd9f2ed1379d60c6
Gilson Cesar Carlos Filho <113089882+TheFireMonster@users.noreply.github.com>
2024-04-26 22:12:57 -0300

    update index.ts config sequelize .env

--- models/index.ts
+++ models/index.ts
@@ -8,7 +8,7 @@ const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env]
 const db: any = {};
 
 const sequelize = config.use_env_variable ?
-  new Sequelize(process.env[config.use_env_variable], config) :
+  new Sequelize(config):
   new Sequelize(config.database, config.username, config.password, config);
 
 
